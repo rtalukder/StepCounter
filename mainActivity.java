3d216a4d6277022de3feecd6c4ccfc69b0e52525
cwholmes <cholmes5324@gmail.com>
2016-09-18 20:14:38 -0500

    Update mainActivity.java

--- mainActivity.java
+++ mainActivity.java
@@ -1,23 +1,98 @@
 package com.example.cody.homework1;
 
+import java.util.ArrayList;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.view.View;
-import android.widget.EditText;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener ,
+        OnClickListener {
+    private SensorManager _sensorManager;
+    private Button btnStart, btnStop;
+    private boolean started = false;
+    private ArrayList sensorData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        _sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorData = new ArrayList();
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStop = (Button) findViewById(R.id.btnStop);
+        btnStart.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+        btnStart.setEnabled(true);
+        btnStop.setEnabled(false);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (started == true){
+            _sensorManager.unregisterListener(this);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (started){
+            double x = event.values[0];
+            double y = event.values[1];
+            double z = event.values[2];
+            long timestamp = System.currentTimeMillis();
+            AccelData data = new AccelData(timestamp, x, y, z);
+            sensorData.add(data);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnStart:
+                btnStart.setEnabled(false);
+                btnStop.setEnabled(true);
+                sensorData = new ArrayList();
+                // save prev data if available]
+                started = true;
+                Sensor accel = _sensorManager
+                        .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                _sensorManager.registerListener(this, accel,
+                        SensorManager.SENSOR_DELAY_FASTEST);
+                break;
+            case R.id.btnStop:
+                btnStart.setEnabled(true);
+                btnStop.setEnabled(false);
+                started = false;
+                break;
+            default:
+                break;
+        }
 
     }
 
     /** Called when the user clicks the Send button */
-    public void startWalk(View view) {
+   /** public void startWalk(View view) {
         Intent intent = new Intent(this, sensorRun.class);
         startActivity(intent);
-    }
+    }**/
 }
